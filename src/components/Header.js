d1edce53ce710bacc16f647a4e2f20151a32fdde
Nameer Nihad <nameernihadpc@gmail.com>
2023-05-31 16:42:33 +0530

    basic class and function components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
- function Header(props){
-    return (<h1>Hello I am {props.data}</h1>)
+ import {Component} from 'react'
+
+class Header extends Component{
+
+   render(){
+      return (<h1>Hello I am {this.componentDidMountprops.data}</h1>)
+   }
+    
  }
 
  export default Header;
